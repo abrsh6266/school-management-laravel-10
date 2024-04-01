c866b8d8bb5f1b2619e4d194aab7ca4f869e6fb8
Abrham <abrsh6266@gmail.com>
2024-03-31 23:21:08 -0800

    creating model and migration for subject_class

---
+++ database/migrations/2024_04_01_071612_create_class_subject_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('class_subject', function (Blueprint $table) {
+            $table->id();
+            $table->integer('class_id')->nullable();
+            $table->integer('subject_id')->nullable();
+            $table->timestamps();
+            $table->integer('created_by')->nullable();
+            $table->integer('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('class_subject');
+    }
+};
